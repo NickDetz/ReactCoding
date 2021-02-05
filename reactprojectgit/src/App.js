6261129a5473a1f3cfb4d347022d0a9617bcaa29
Deters <nicholasdeters47@live.com>
2021-02-05 10:00:46 -0800

    React playing around with the App.js

--- reactprojectgit/src/App.js
+++ reactprojectgit/src/App.js
@@ -1,13 +1,20 @@
-import React, {Component} from 'react';
+import React, {Fragment, Component} from 'react';
 import './App.css';
 
 class App extends Component {
 
   render(){
+
+    const name = 'John Doe';
+
+    
     return (
+      <Fragment>
       <div className="App">
       <h1> Hello React </h1>
+      <h2>My H2 title {name.toUpperCase()} {name.toLowerCase()} {name}</h2>
       </div>
+      </Fragment>
     );
 
   }
